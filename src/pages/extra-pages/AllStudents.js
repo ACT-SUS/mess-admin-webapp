4ac446cc1c966243527b60603e0400286b228bcd
Asavari Ambavane <ambavaneasavari@gmail.com>
2023-01-14 02:17:51 +0530

    export to csv done

--- src/pages/extra-pages/AllStudents.js
+++ src/pages/extra-pages/AllStudents.js
@@ -7,55 +7,76 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Button from "@mui/material/Button";
-import { all_student_data } from "./all_student_data";
-import axios from 'axios'
+import axios from "axios";
+import { Grid } from "@mui/material";
+import exportCSVFile from "./export_to_csv";
+import DownloadIcon from "@mui/icons-material/Download";
 
 const AllStudents = () => {
-    const [students, setStudents] = useState([])
+  const [students, setStudents] = useState([]);
 
-    useEffect(() => {
-        try {
-            (async () => {
-                const data = await axios.get('http://localhost:5000/api/student')
-                setStudents(data.data.students);
-                console.log(data.data.students);
-            })()
-        } catch (error) {
-            console.error(error);
-        }
-    }, [])
+  var headers = {
+    sid: "Roll Num",
+    name: "Name",
+    balance: "Balance",
+    expense: "Expense",
+  };
 
-    return (
-        <MainCard>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="left">Roll Num</TableCell>
-                            <TableCell align="left">Name</TableCell>
-                            <TableCell align="left">Balance</TableCell>
-                            <TableCell align="left">Expense</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {students.map((x, y) => {
-                            return (
-                                <TableRow
-                                    key={y}
-                                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                                >
-                                    <TableCell align="left">{x.id ? x.id : x.sid}</TableCell>
-                                    <TableCell align="left">{`${x.firstName} ${x.lastName}`}</TableCell>
-                                    <TableCell align="left">{x.balance}</TableCell>
-                                    <TableCell align="left">{x.expense}</TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </MainCard>
-    );
+  var fileTitle = "student_data"; // or 'my-unique-title'
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const data = await axios.get("http://localhost:5000/api/student");
+        setStudents(data.data.students);
+        console.log(data.data.students);
+      })();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <MainCard>
+      <Grid container justifyContent="end">
+        <Button
+          variant="outlined"
+          color="warning"
+          endIcon={<DownloadIcon />}
+          onClick={() => exportCSVFile(headers, students, fileTitle)}
+        >
+          Export To CSV
+        </Button>
+      </Grid>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Roll Num</TableCell>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="left">Balance</TableCell>
+              <TableCell align="left">Expense</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map((x, y) => {
+              return (
+                <TableRow
+                  key={y}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="left">{x.id ? x.id : x.sid}</TableCell>
+                  <TableCell align="left">{`${x.firstName} ${x.lastName}`}</TableCell>
+                  <TableCell align="left">{x.balance}</TableCell>
+                  <TableCell align="left">{x.expense}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </MainCard>
+  );
 };
 
 export default AllStudents;
